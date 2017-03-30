6245a941797d7b53cc957ac6e496bf02a7fb6815
Igor Fontana <igor.fontana@hotmail.com>
2017-03-30 16:14:19 -0300

    Refactor of filter, better way to manipulate and reuse

--- imports/ui/components/messageList/messageList.js
+++ imports/ui/components/messageList/messageList.js
@@ -1,12 +1,12 @@
 import {Template} from "meteor/templating";
 import {Messages} from "../../../api/messages/messages.js";
-import {commonFilter as messagesFilter} from "../../../api/messages/filters";
+import {MessagesFilter} from "../../../api/messages/filters";
 import "./messageList.html";
 
 Template.messageList.helpers({
 
     messages () {
-        return Messages.find({}, messagesFilter);
+        return Messages.find({}, MessagesFilter.common());
     }
 
 });
