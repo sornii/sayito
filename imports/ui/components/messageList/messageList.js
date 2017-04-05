c017ec10435cd4328ef7cf9a68d4e4d727a33d97
Igor Fontana <igor.fontana@hotmail.com>
2017-04-05 01:55:30 -0300

    :sparkles: Possibility to infinite scroll

--- imports/ui/components/messageList/messageList.js
+++ imports/ui/components/messageList/messageList.js
@@ -1,28 +1,53 @@
+import {Meteor} from 'meteor/meteor';
 import {Template} from "meteor/templating";
 import {ReactiveDict} from "meteor/reactive-dict";
 import {ReactiveVar} from "meteor/reactive-var";
 import {Messages} from "../../../api/messages/messages.js";
 import {MessagesFilter} from "../../../api/messages/filters";
+import { Tracker } from 'meteor/tracker'
 import "../../components/dummy/dummy.js";
 import "./messageList.html";
 
-Template.messageList.onCreated(function onCreated() {
+Template.messageList.onCreated(function messageListOnCreated() {
+    const limit = 'limit';
+    const limitStep = 5;
+
     this.state = new ReactiveDict();
     this.state.setDefault({
-        limit: 5
+        [limit]: limitStep
     });
 
-    this.autorun(() => {
-        this.subscribe('messages', this.state.get('limit'));
-    });
+    this.getLimit = () =>
+        this.state.get(limit);
 
+    this.increaseLimit = () =>
+        this.state.set(limit, this.getLimit() + limitStep);
+
+    Tracker.autorun(() => {
+        const limitValue = this.getLimit();
+        this.subscribe('messages', limitValue);
+    });
+});
+
+Template.messageList.onRendered(function messageListOnRendered() {
+
+    const instance = this;
+
+    $('.ui.feed.sayito.messages')
+        .visibility({
+            once: false,
+            initialCheck: false,
+            onBottomVisible () {
+                instance.increaseLimit();
+            }
+        });
 });
 
 Template.messageList.helpers({
 
     messages () {
         const instance = Template.instance();
-        return Messages.find({}, MessagesFilter.common({limit: instance.state.get('limit')}));
+        return Messages.find({}, MessagesFilter.common({limit: instance.getLimit()}));
     }
 
 });
