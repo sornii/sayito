da4aa3a195ba32999931dae925a8e403c42896fe
Igor Fontana <igor193@gmail.com>
2017-08-23 20:17:20 -0300

    Lowers the complexity of insert message method

--- imports/api/messages/methods.js
+++ imports/api/messages/methods.js
@@ -5,39 +5,42 @@ import { ValidatedMethod } from 'meteor/mdg:validated-method';
 import SimpleSchema from 'simpl-schema';
 import { TAPi18n } from 'meteor/tap:i18n';
 
+import { LoggedInMixin } from '../../utils/loggedin-mixin';
 import { Messages } from './messages';
 import { Tags } from '../tags/tags';
 import { findChecked as findThreadChecked } from '../threads/methods';
 
+function extractTagsFromText(text) {
+  const regex = /\B#(\w*[A-Za-z_]+\w*)/g;
+  const tags = [];
+
+  let match = regex.exec(text);
+  while (match !== null) {
+    const tagText = match[0];
+    const tag = Tags.findOne({ text: tagText });
+
+    if (tag) {
+      tags.push(tag._id);
+    } else {
+      tags.push(Tags.insert({ text: tagText }));
+    }
+
+    match = regex.exec(text);
+  }
+
+  return tags;
+}
+
 export const insert = new ValidatedMethod({
   name: 'messages.insert',
+  mixins: [LoggedInMixin],
   validate: new SimpleSchema({
     text: { type: String, max: 140 },
     thread: { type: String, optional: true },
     password: { type: String, optional: true },
   }).validator(),
   run({ text, thread, password }) {
-    if (!this.userId) {
-      throw new Meteor.Error('not-authorized');
-    }
-
-    const regex = /\B#(\w*[A-Za-z_]+\w*)/g;
-
-    const tags = [];
-
-    let match = regex.exec(text);
-    while (match != null) {
-      const tagText = match[0];
-      const tag = Tags.findOne({ text: tagText });
-
-      if (tag) {
-        tags.push(tag._id);
-      } else {
-        tags.push(Tags.insert({ text: tagText }));
-      }
-
-      match = regex.exec(text);
-    }
+    const tags = extractTagsFromText(text);
 
     if (tags.length === 0) {
       throw new Meteor.Error('messages.insert.hasNoTags', TAPi18n.__('messages_no_tags'));
