1daaeb95ad7201cd90e113852aed2cd6ac30f822
Igor Fontana <igor193@gmail.com>
2017-08-23 21:41:27 -0300

    Simplified use of arrow function

--- imports/api/threads/server/publications.js
+++ imports/api/threads/server/publications.js
@@ -1,6 +1,5 @@
 import { Meteor } from 'meteor/meteor';
 import { Threads } from '../threads';
 
-Meteor.publish('threads', ({ name = null, password = null }) => {
-  return Threads.find({ name, password });
-});
+Meteor.publish('threads', ({ name = null, password = null }) =>
+  Threads.find({ name, password }));
