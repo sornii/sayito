69a7c8b4360e0f9445d92f8470755a9d6a234e32
Igor Fontana <igor193@gmail.com>
2017-08-24 00:27:26 -0300

    Lowers the complexity of annons.js

--- imports/startup/server/annon.js
+++ imports/startup/server/annon.js
@@ -1,6 +1,19 @@
 import { Meteor } from 'meteor/meteor';
 import { Accounts } from 'meteor/accounts-base';
 
+function findUserId(annonId) {
+  let userId;
+
+  const annon = Meteor.users.findOne({ annonId });
+  if (!annon) {
+    userId = Meteor.users.insert({ annonId });
+  } else {
+    userId = annon._id;
+  }
+
+  return userId;
+}
+
 Accounts.registerLoginHandler((loginRequest) => {
   if (!loginRequest.annon) {
     return undefined;
@@ -12,16 +25,6 @@ Accounts.registerLoginHandler((loginRequest) => {
     return null;
   }
 
-  let userId;
-
-  const annon = Meteor.users.findOne({ annonId });
-  if (!annon) {
-    userId = Meteor.users.insert({ annonId });
-  } else {
-    userId = annon._id;
-  }
-
-  return {
-    userId,
-  };
+  const userId = findUserId(annonId);
+  return { userId };
 });
