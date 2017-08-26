e96a134f9a126a54cd33ff1658e6246904fae8f6
Igor Fontana <igor193@gmail.com>
2017-08-26 08:54:20 -0300

    Adds an automatic bump on travis

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,16 @@
-var gulp = require('gulp');
-var bump = require('gulp-bump');
+var gulp = require('gulp'),
+  git = require('gulp-git'),
+  bump = require('gulp-bump'),
+  tag_version = require('gulp-tag-version');
 
-gulp.task('bump', function onBump() {
-  gulp.src('./package.json')
-    .pipe(bump())
-    .pipe(gulp.dest('./'));
-});
+function inc(importance) {
+  return gulp.src('./package.json')
+    .pipe(bump({ type: importance }))
+    .pipe(gulp.dest('./'))
+    .pipe(git.commit('bumps package version'))
+    .pipe(tag_version());
+}
+
+gulp.task('patch', function () { return inc('patch'); });
+gulp.task('feature', function () { return inc('minor'); });
+gulp.task('release', function () { return inc('major'); });
