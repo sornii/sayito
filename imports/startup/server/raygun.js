797415680735d1100807efa3730c8b7c53869c2a
Igor Fontana <igor193@gmail.com>
2017-08-26 01:45:16 -0300

    Import raygun to index, changes require to import

--- imports/startup/server/raygun.js
+++ imports/startup/server/raygun.js
@@ -1,8 +1,9 @@
 import raygun from 'raygun';
+import domain from 'domain';
 
 var raygunClient = new raygun.Client().init({ apiKey: 'Sah7EBMKsi8rNnTMEmiJjA==' });
 
-var d = require('domain').create();
+var d = domain.create();
 d.on('error', function(err){
   raygunClient.send(err, {}, function () {
     process.exit();
